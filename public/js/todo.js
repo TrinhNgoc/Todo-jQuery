295132a4ecc8c7babeba841dc4ee1146282eca84
Jenny <jennyngu@hawaii.edu>
2014-12-04 21:59:49 -1000

    Striked out

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,8 +1,25 @@
 $(function(){
-  $( "#input" ).keydown(function(e){
+
+  var input = $( "#input" );
+
+  input.keydown(function(e){
     if( e.keyCode == 13 ){
-      var newListItem = $("#input").val();
-      $( ".list" ).append( "<li>" + newListItem + "</li>" );
+      var newListItem = input.val();
+      var checkbox = "<input type=checkbox>";
+      $( ".list" ).append( "<li>" + checkbox + newListItem + "</li>" );
+      $( this ).val(""); 
+      // $( "li" ).append( "<input type=checkbox>" );
     }
   });
+
+  $('body').on("click", "input:checkbox", function() {
+    if(this.checked) {
+      // alert("Checked!");
+      $(this).parent().css("text-decoration","line-through");
+    }
+    else {
+      // alert("Unchecked!");
+      $(this).parent().css("text-decoration","none");
+    }
+  })
 });
