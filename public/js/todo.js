3512ed864d46032fa4facc375a33eb85261af30f
Jenny <jennyngu@hawaii.edu>
2014-12-11 18:36:38 -1000

    Counter sort of works and added a post request

--- public/js/todo.js
+++ public/js/todo.js
@@ -2,11 +2,12 @@ $(function(){
 
   var input = $( "#input" );
 
+
   input.keydown(function(e){
     if( e.keyCode == 13 ){
       var newListItem = input.val();
       var checkbox = "<input type=checkbox>";
-      $( ".list" ).append( "<li>" + checkbox + newListItem + "</li>" );
+      $( ".list" ).append( "<li><span>" + checkbox + newListItem + "</span></li>" );
       $( this ).val(""); 
       // $( "li" ).append( "<input type=checkbox>" );
     }
@@ -16,15 +17,49 @@ $(function(){
     if(this.checked) {
       // alert("Checked!");
       $(this).parent().css("text-decoration","line-through");
+      var checkedCheckbox = ($("input:checked").length);
+      $(".counter").text(checkedCheckbox + " items completed");
     }
     else {
       // alert("Unchecked!");
       $(this).parent().css("text-decoration","none");
+      var checkedCheckbox = ($("input:checked").length);
+      var totalCheckbox = $("input:checkbox").length;
+      var uncheckedbox = totalCheckbox - checkedCheckbox;
+      $(".items-left").text( uncheckedbox + (uncheckedbox === 0 ? " item" : " items") + " left");
     }
   })
 
-  var totalCheckbox = $("input:checkbox").length;
-  var checked = $("input:checkbox:checked").length;
+  // var itemsLeft = function () {
+  //   var totalCheckbox = $("input:checkbox").length;
+  //   $(".list").html( "<div>" + totalCheckbox + (totalCheckbox === 1 ? " item" : " items") + " left" + "</div>");
+  // };
+  // itemsLeft();
 
-  $('.list-box').append(totalCheckbox);
+  // var checkedCheckbox = $("input:checkbox:checked").length;
+
+  // $('.list-box').append(totalCheckbox);
+
+  $('button#save').click(function(e){
+    var list = [];
+    $("span").each(function(i, obj) {
+      list.push({
+        index : i,
+        title : $(obj).find("span").html(),
+        completed: $(obj).find("input:checked").length > 0
+      });
+    });
+
+
+    var json = JSON.stringify(list);
+    console.log(json);
+
+    $.post("/save", 
+      {
+        todo_json_data : json
+      }
+    );
+  }); 
+
+
 });
---
+++ server/app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+// })
+
+app.post('/save', function (req, res) {
+
+  console.log("user sent post request");
+  res.send("ok");
+   
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+});
+
+// fs = require('fs');
+// fs.writeFile('./meow', 'Hello World!', function (err) {
+//   if (err) return console.log(err);
+//   console.log('Hello World > index.html');
+// });
+//If filename is not define, it will create a new file
